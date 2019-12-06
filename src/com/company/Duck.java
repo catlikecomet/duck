6ebaf26a49dd1ac2fb621e12d77292721e1355ed
Cameron-Jade Mummery <cami.mummery@aexp.com>
2019-12-06 09:57:47 +0000

    duck work

---
+++ src/com/company/Duck.java
@@ -0,0 +1,72 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class Duck implements Comparable<Duck>{
+    private String name;
+    private String type;
+    private int weightInGrams;
+    private int ageInMonths;
+
+    public Duck(String name, String type, int weightInGrams, int ageInMonths) {
+        this.name = name;
+        this.type = type;
+        this.weightInGrams = weightInGrams;
+        this.ageInMonths = ageInMonths;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getWeightInGrams() {
+        return weightInGrams;
+    }
+
+    public int getAgeInMonths() {
+        return ageInMonths;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if(o.getClass() != o.getClass()) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public int compareTo(Duck other) {
+        return name.compareTo(other.name);
+    }
+
+
+    public static void main(String[] args) {
+
+        List<Duck> ducks = Arrays.asList(
+                new Duck("Sir Quackalot", "Mallard", 100, 17),
+                new Duck("Duckington","flap bird", 250, 120)
+        );
+
+
+        Collections.sort(ducks);
+        ducks.forEach(duck -> System.out.println(duck.name + " " + duck.type + " " + duck.weightInGrams + " " + duck.ageInMonths));
+
+
+
+
+    }
+}
