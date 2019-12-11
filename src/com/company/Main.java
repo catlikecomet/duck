c602d437fc4a9225f0097b4b113b5852c8e7f55d
Cameron-Jade Mummery <cami.mummery@aexp.com>
2019-12-11 14:43:37 +0000

    changing a value name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        HashMap<Duck, String> duck = new HashMap<Duck, String>();
+        HashMap<Duck, String> duck = new HashMap<>();
 
         duck.put(new Duck("Sir Quackalot","Mallard", 100, 17), "duck1");
-        duck.put(new Duck("Trevoir", "Birb", 1000, 216), "thatBitch");
+        duck.put(new Duck("Trevoir", "Birb", 1000, 216), "duck3");
         duck.put(new Duck("Duckington","Flap Bird", 250, 120), "duck2");
 
         //Sorting the hashmap by using a treemap
